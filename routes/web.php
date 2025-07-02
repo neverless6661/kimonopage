f09de603571e66c0c1ecbff59a0d58a868a28009
LuBani <lsca_lubani@hotmail.com>
2025-07-02 15:28:56 -0600

    cambio en rutas

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,18 @@
 use App\Http\Controllers\PaginaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PaginaController::class, 'inicio'])->name('inicio');
-Route::get('/nosotros', [PaginaController::class, 'inicio'])->name('nosotros');
-Route::get('/servicios', [PaginaController::class, 'inicio'])->name('servicios');
-Route::get('/contacto', [PaginaController::class, 'inicio'])->name('contacto');
 
+Route::get('/', [PaginaController::class, 'inicio'])->name('inicio');
+Route::get('/nosotros', [PaginaController::class, 'nosotros'])->name('nosotros');
+Route::get('/servicios', [PaginaController::class, 'servicios'])->name('servicios');
+Route::get('/contacto', [PaginaController::class, 'contacto'])->name('contacto');
+
+/*
+Route::get('/', 'PaginaController@inicio')->name('inicio');
+Route::get('/nosotros', 'PaginaController@nosotros')->name('nosotros');
+Route::get('/servicios', 'PaginaController@servicios')->name('servicios');
+Route::get('/contacto', 'PaginaController@contacto')->name('inicio');
+*/
 
 
 
